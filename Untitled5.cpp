961f1d84c98422b0c8d74e2a08b803d4b28bae1b
dung27-vi <duvi57836@gmail.com>
2025-10-16 21:37:23 +0700

    BTVN04

---
+++ Untitled5.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int chieu_dai = 10;
+    int chieu_rong = 5;
+
+    int chu_vi = (chieu_dai + chieu_rong) * 2;
+    int dien_tich = chieu_dai * chieu_rong;
+
+    printf("Chu vi hinh chu nhat la: %d\n", chu_vi);
+    printf("Dien tich hinh chu nhat la: %d\n", dien_tich);
+
+    return 0;
+}
