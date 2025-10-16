cf704806886d9cd563140777ad439a703351dd1b
dung27-vi <duvi57836@gmail.com>
2025-10-16 21:30:09 +0700

    Add files via upload

---
+++ Untitled3.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Khai báo và kh?i t?o hai bi?n s? nguyên
+    int so_a = 5;
+    int so_b = 10;
+
+    // Tính t?ng c?a hai bi?n và lýu k?t qu?
+    int tong = so_a + so_b;
+
+    // In k?t qu? ra màn h?nh (tùy ch?n)
+    printf("Tong cua %d va %d la: %d\n", so_a, so_b, tong);
+
+    return 0;
+}
+
